d34f9b7573116d3747edcd77cf3266b0cf13e7b5
wudejian789 <33439004+wudejian789@users.noreply.github.com>
2020-05-21 11:28:00 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -253,7 +253,7 @@ class DataClass_BP:
             vecPath = f'cache/{method}_k{self.k}_d{feaSize}.pkl'
         if os.path.exists(vecPath) and loadCache:
             with open(vecPath, 'rb') as f:
-                self.vector['embedding'] = pickle.load(f)
+                self.vector['embedding' if method!='feaEmbedding' else 'feaEmbedding'] = pickle.load(f)
             print(f'Loaded cache from cache/{vecPath}.')
             return
         if method == 'feaEmbedding':
